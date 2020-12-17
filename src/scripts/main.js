dc73a2d3a4e995ea82909b5bcf28ce5e231a814e
Rafi Indrajati <99.rafiindrajati@gmail.com>
2020-12-17 15:33:38 +0700

    git init

---
+++ src/scripts/main.js
@@ -0,0 +1,69 @@
+function main() {
+
+    const baseUrl = "https://api.themoviedb.org/3/list/1?api_key=501768670b68312d371b3a1143ac74be&language=en-US";
+
+    const getmovie = () => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.onload = function () {
+            debugger
+            const responseJson = JSON.parse(this.responseText);
+            if (responseJson.error) {
+                showResponseMessage(responseJson.message);
+            } else {
+                renderAllMovie(responseJson.items);
+            }
+        }
+
+        xhr.onerror = function () {
+            showResponseMessage();
+        }
+
+        xhr.open("GET", `${baseUrl}`);
+        xhr.send();
+    };
+
+    const renderAllMovie = (items) => {
+        const listMovieElement = document.querySelector("#listMovie");
+        listMovieElement.innerHTML = "";
+
+        items.forEach(item => {
+            listMovieElement.innerHTML += `
+                <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 style='font-size:15px;'>ID: ${item.id}<br>Title: ${item.title}</h5>
+                            <p>${item.overview.slice(0, 200)}...</p>
+                            <p>Release Date: ${item.release_date}</p>
+                            <p>Rate : ${item.popularity}%</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+
+    };
+
+    const renderAllMovie = (items) => {
+        const listMovieElement = document.querySelector("#sortMovie");
+        listMovieElement.innerHTML = "";
+
+        
+    }
+
+    const showResponseMessage = (message = "Check your internet connection") => {
+        alert(message);
+    };
+
+    document.addEventListener("DOMContentLoaded", () => {
+
+        const inputMovieId = document.querySelector("#inputMovieId");
+        const inputMovieTitle = document.querySelector("#inputMovieTitle");
+        const inputBookOverview = document.querySelector("#inputBookOverview");
+        const inputBookPopularity = document.querySelector("#inputBookPopularity");
+        
+        getmovie();
+    });
+}
+
+export default main;
