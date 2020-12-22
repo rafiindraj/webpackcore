a87ba98ad4f633a3d3319ff0f643dc8e220ec35f
Rafi Indrajati <Rafi Indrajati>
2020-12-22 21:57:28 +0700

    add date custom element to js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,39 +23,53 @@ function main() {
         xhr.send();
     };
 
-    class listMovie extends HTMLElement {
-        connectedCallback() {
-            this.renderAllMovie();
-        }
-        renderAllMovie(item){
-            const listMovieElement = document.querySelector("#listMovie");
-            listMovieElement.innerHTML = "";
 
-            items.forEach(item => {
-                listMovieElement.innerHTML += `
-                    <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
-                        <div class="card">
-                            <div class="card-body content-body">
-                                <h5 style='font-size:15px;'>ID: ${item.id}<br>Title: ${item.title}</h5>
-                                <p>${item.overview.slice(0, 200)}...</p>
-                                <p>Release Date: ${item.release_date}</p>
-                                <p>Rate : ${item.popularity}%</p>
-                            </div>
+    class TimeFormatted extends HTMLElement { // (1)
+
+        connectedCallback() {
+            let date = new Date(this.getAttribute('datetime') || Date.now());
+
+            this.innerHTML = new Intl.DateTimeFormat("default", {
+                year: this.getAttribute('year') || undefined,
+                month: this.getAttribute('month') || undefined,
+                day: this.getAttribute('day') || undefined,
+                hour: this.getAttribute('hour') || undefined,
+                minute: this.getAttribute('minute') || undefined,
+                second: this.getAttribute('second') || undefined,
+                timeZoneName: this.getAttribute('time-zone-name') || undefined,
+            }).format(date);
+        }
+
+    }
+
+    customElements.define("time-formatted", TimeFormatted); // (2)
+    const renderAllMovie = (items) => {
+        const listMovieElement = document.querySelector("#listMovie");
+        listMovieElement.innerHTML = "";
+
+        items.forEach(item => {
+            listMovieElement.innerHTML += `
+                <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
+                    <div class="card">
+                        <div class="card-body content-body">
+                            <h5 style='font-size:15px;'>ID: ${item.id}<br>Title: ${item.title}</h5>
+                            <p>${item.overview.slice(0, 200)}...</p>
+                            <p>Release Date: ${item.release_date}</p>
+                            <p>Rate : ${item.popularity}%</p>
                         </div>
                     </div>
-                `;
-            });
+                </div>
+            `;
+        });
 
-        };
     };
-    customElements.define("listMovie", listMovie);
 
     const showResponseMessage = (message = "Check your internet connection") => {
         alert(message);
     };
 
     document.addEventListener("DOMContentLoaded", () => {
-        
+
         getmovie();
     });
 }
